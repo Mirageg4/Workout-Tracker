4131d493f1cfd42c32dfe98c9933b0fc885e6beb
John Manouse <johnmanouse@Macintosh-2.local>
2020-09-09 09:00:58 -0400

    update server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(logger("dev"));
+
+//Use Middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Static Assets (used with Heroku)
+app.use(express.static("public"));
+
+//api routes
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+//Connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, ()=> {
+    console.log(`App running on port ${PORT}!`);
+});
